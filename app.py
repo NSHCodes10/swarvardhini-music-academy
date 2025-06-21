911563da6d609e8c6e9599edb46771d8935cb6bd
Nitin_Hayatnagarkar <153418434+NSHCodes10@users.noreply.github.com>
2025-06-21 11:15:42 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -19,4 +19,6 @@ def contact():
     return render_template("contact.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000)) # Render sets PORT env variable
+    app.run(host="0.0.0.0", port=port)
+    
